8eda6fc53eed5f91ce0c5d55d490efe55b1feec6
Vedant Wankhade <vedantwankhade47@gmail.com>
2025-09-28 15:53:40 +0530

    [dyad] Enhanced star glowing and blinking effect - wrote 2 file(s)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,6 +36,9 @@ const Hero = () => {
         <a href="#about"><i className="fas fa-chevron-down"></i></a>
       </div>
       <div className="hero-background">
+        <div id="stars1"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
         <div className="shape shape1"></div>
         <div className="shape shape2"></div>
         <div className="shape shape3"></div>
